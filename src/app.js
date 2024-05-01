4821a71e88bc67057a22391b656b0872f3a2a38c
Ardianto Nugroho <ardi19nugroho@gmail.com>
2024-05-01 17:22:58 +0700

    feat: add file example of env

--- src/app.js
+++ src/app.js
@@ -29,14 +29,7 @@ app.use((req, res, next) => {
 
 app.set("trust proxy", false);
 
-app.use(
-  cors({
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
-    optionsSuccessStatus: 201,
-  })
-);
+app.use(cors());
 
 app.use(
   helmet({
