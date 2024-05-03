da47b8d88284774d257b3685f2f0f095a8751299
YYanti00 <lindayanti928@gmail.com>
2024-05-03 16:13:09 +0700

    update pagination in getComments and getStories

--- src/api/controllers/Comments/getComments.js
+++ src/api/controllers/Comments/getComments.js
@@ -5,24 +5,31 @@ export const getComments = async (req = request, res = response) => {
   const { storyId: story_id } = req.params;
 
   const { page = 1, limit = 10 } = await req.query;
-  let skip = (page - 1) * limit;
+  let offset = (page - 1) * limit;
 
   const totalCountStory = await commentService.totalCommentData();
+
   const totalPages = Math.ceil(totalCountStory / limit);
+
+  const next = offset + limit < totalCountStory;
+  const prev = offset > 0;
 
   const result = await commentService.getComments(
     parseInt(story_id),
-    skip,
+    offset,
     limit
   );
 
   return res.status(200).json({
     status: true,
     message: "Get comment successfully",
-    total_data: totalCountStory,
-    limit: parseInt(limit),
-    page: parseInt(page),
-    total_pages: totalPages,
+    pagination: {
+      total_pages: totalPages,
+      limit: parseInt(limit),
+      current_page: parseInt(page),
+      next: next,
+      prev: prev,
+    },
     query: result,
   });
 };
--- src/api/controllers/Story/getStories.js
+++ src/api/controllers/Story/getStories.js
@@ -1,21 +1,29 @@
 import { request, response } from "express";
 import storyService from "../../../lib/services/Story";
+
 export const getStories = async (req = request, res = response) => {
   const { page = 1, limit = 10 } = await req.query;
-  let skip = (page - 1) * limit;
+  let offset = (page - 1) * limit;
 
   const totalCountStory = await storyService.totalStoryData();
+
   const totalPages = Math.ceil(totalCountStory / limit);
 
-  const result = await storyService.getStory(skip, limit);
+  const next = offset + limit < totalCountStory;
+  const prev = offset > 0;
+
+  const result = await storyService.getStory(offset, limit);
 
   return res.status(200).json({
     status: true,
     message: "Get story successfully",
-    total_data: totalCountStory,
-    limit: parseInt(limit),
-    page: parseInt(page),
-    total_pages: totalPages,
+    pagination: {
+      total_pages: totalPages,
+      limit: parseInt(limit),
+      current_page: parseInt(page),
+      next: next,
+      prev: prev,
+    },
     query: result,
   });
 };
