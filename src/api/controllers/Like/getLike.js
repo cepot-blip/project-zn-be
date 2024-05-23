531a672e7e7e9098d643197ad55249ce80151a94
YYanti00 <lindayanti928@gmail.com>
2024-05-23 22:30:34 +0700

    update the getLike data retrieval and add authorization check when delete restory

--- src/api/controllers/Like/getLike.js
+++ src/api/controllers/Like/getLike.js
@@ -5,14 +5,17 @@ import NotFoundError from "../../../utils/exceptions/NotFoundError";
 import tokenize from "../../../utils/tokenize";
 
 export const getLike = async (req = request, res = response) => {
-  const { storyId: story_id } = req.params;
+  const story_id = parseInt(req.params.storyId);
 
-  const checkStoryId = await storyService.getStoryById(parseInt(story_id));
+  const jwtToken = req.headers.authorization;
+  const { id: user_id } = await tokenize.decodeJWT(jwtToken);
+
+  const checkStoryId = await storyService.getStoryById(story_id);
   if (!checkStoryId) {
     throw new NotFoundError("Story not found, put valid ID");
   }
 
-  const result = await likeService.getStoryLikes(parseInt(story_id));
+  const result = await likeService.checkAvailabilityLike(user_id, story_id);
 
   return res.status(201).json({
     status: true,
--- src/api/controllers/Restory/deleteReStory.js
+++ src/api/controllers/Restory/deleteReStory.js
@@ -1,16 +1,27 @@
 import { request, response } from "express";
 import NotFoundError from "../../../utils/exceptions/NotFoundError";
 import reStoryService from "../../../lib/services/ReStory";
+import tokenize from "../../../utils/tokenize";
+import AuthorizationError from "../../../utils/exceptions/AuthorizationError";
 
 export const deleteReStory = async (req = request, res = response) => {
-  const { id } = req.params;
+  const id = parseInt(req.params.id);
 
-  const checkReStoryId = await reStoryService.getReStoryById(parseInt(id));
+  const jwtToken = req.headers.authorization;
+  const { id: user_id } = await tokenize.decodeJWT(jwtToken);
+
+  const checkReStoryId = await reStoryService.getReStoryById(id);
   if (!checkReStoryId) {
     throw new NotFoundError("ReStory not found, put valid ID");
   }
 
-  await reStoryService.deleteReStory(parseInt(id));
+  if (user_id !== checkReStoryId.user_id) {
+    throw new AuthorizationError(
+      "You are not authorized to delete this reStory"
+    );
+  }
+
+  await reStoryService.deleteReStory(id);
 
   return res.status(200).json({
     status: true,
